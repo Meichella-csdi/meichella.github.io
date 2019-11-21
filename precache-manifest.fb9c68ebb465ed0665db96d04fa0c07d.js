5910a0abdc1362b6a4fa5399b310a3a5c6b74fe9
meichella <=meichella4599@gmail.com>
2019-11-21 12:21:59 +0700

    init pertama

---
+++ precache-manifest.fb9c68ebb465ed0665db96d04fa0c07d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f7a1ee1b7dc34e23fa",
+    "url": "/css/about.0a539726.css"
+  },
+  {
+    "revision": "2133bab8e8658002a726",
+    "url": "/css/chunk-vendors.41c9e408.css"
+  },
+  {
+    "revision": "01c9ab58115b39abcae9c95ada19f417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6f7a1ee1b7dc34e23fa",
+    "url": "/js/about.12f83edf.js"
+  },
+  {
+    "revision": "c75302f051aa61f9a856",
+    "url": "/js/app.c3aba567.js"
+  },
+  {
+    "revision": "2133bab8e8658002a726",
+    "url": "/js/chunk-vendors.91390cb5.js"
+  },
+  {
+    "revision": "ea9e354afda91143ff7e194948c76957",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
